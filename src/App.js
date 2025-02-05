5eb0824d3ace89361efe07cf4215b6d47092b7bc
kimjungyeon <kim_jungyeon@pulio365.com>
2025-02-05 14:03:29 +0900

    remove source

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
 const Gift = React.lazy(() => import('./components/gift/Gift'));
-const Station = React.lazy(() => import('./components/station/Station'));
 const Marketing = React.lazy(() => import('./components/marketing/Marketing'));
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Gift></Gift>}></Route>
           <Route path="/marketing" element={<Marketing></Marketing>}></Route>
-          <Route path="/station" element={<Station></Station>}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
         <Footer></Footer>
